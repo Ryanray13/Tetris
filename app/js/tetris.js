d11fd38eb2e1221203589986fe36631c99eeeb27
Wuping Lei <wuping.lei@nyu.edu>
2015-05-05 14:34:16 -0400

    fix countDown bug

--- app/js/tetris.js
+++ app/js/tetris.js
@@ -101,11 +101,9 @@ function createCanvasController(canvas) {
     matchController = params.matchController;
     //isGameOngoing = true;
     isSinglePlayer = playersInfo.length === 1;
-    if (isSinglePlayer) {
-      isSinglePlayerGlobal = isSinglePlayer;
-      totalPauseTime = 0;
-      pauseStartTime = 0;
-    }
+    isSinglePlayerGlobal = isSinglePlayer;
+    totalPauseTime = 0;
+    pauseStartTime = 0;
 
     pieceCreatedNum = 0;
     allScores = [];
